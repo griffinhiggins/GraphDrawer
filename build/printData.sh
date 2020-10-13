ff995ec5e60d94a03db799a8a44edc9580da44b4
Griffin Higgins <griffin.higgins@unb.ca>
2020-10-13 14:28:54 -0300

    added runnable .jar and removed bins and data

---
+++ build/printData.sh
@@ -0,0 +1,17 @@
+echo "----------------------DATA---------------------------"
+cat ./data/edgeLists/physical.txt
+echo " "
+cat ./data/edgeLists/cyber.txt
+echo " "
+cat ./data/edgeLists/hybrid.txt
+echo " "
+cat ./data/matricies/physical.txt
+echo " "
+cat ./data/matricies/cyber.txt
+echo " "
+cat ./data/matricies/hybrid.txt
+echo " "
+cat ./data/meta/nodes.txt
+echo " "
+cat ./data/meta/stats.txt
+echo "-----------------------------------------------------"
